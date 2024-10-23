96650b1645a2f2945ed39b3aed35128a29c392f5
tat <tatsu0823takasago@icloud.com>
2024-10-23 15:37:03 +0900

    new

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
-export {};
+// export {};
 
-declare global {
-  interface Window {
-    filterArticles: (tag: string) => void;
-    handleTagClick: (tag: string) => void;
-    filteredArticles: Array<{ 
-      id: string;
-      title: string;
-      thumbnail: { url: string };
-      tag: string;
-      publishedAt: string;
-    }>;
-    filterArticles: (tag: string) => void;
-  },
-}
+// declare global {
+//   interface Window {
+//     filterArticles: (tag: string) => void;
+//     handleTagClick: (tag: string) => void;
+//     filteredArticles: Array<{ 
+//       id: string;
+//       title: string;
+//       thumbnail: { url: string };
+//       tag: string;
+//       publishedAt: string;
+//     }>;
+//     filterArticles: (tag: string) => void;
+//   },
+// }
